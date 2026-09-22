e8a3f329c6ec0018b5829371b72a20a1f2fcedf7
z354prance <313707038+z354prance@users.noreply.github.com>
2026-09-22 00:09:06 -0500

    Add sanitized public services endpoint

--- internal/api/services.go
+++ internal/api/services.go
@@ -22,6 +22,36 @@ type serviceRequest struct {
 	UpdateCredential bool               `json:"update_credential"`
 }
 
+type publicService struct {
+	ID      int64              `json:"id"`
+	Type    models.ServiceType `json:"type"`
+	Name    string             `json:"name"`
+	Enabled bool               `json:"enabled"`
+}
+
+func (a *API) listPublicServices(
+	w http.ResponseWriter,
+	_ *http.Request,
+) {
+	items, err := a.services.List()
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	publicItems := make([]publicService, 0, len(items))
+	for _, item := range items {
+		publicItems = append(publicItems, publicService{
+			ID:      item.ID,
+			Type:    item.Type,
+			Name:    item.Name,
+			Enabled: item.Enabled,
+		})
+	}
+
+	writeJSON(w, http.StatusOK, publicItems)
+}
+
 func (a *API) serviceTypes(
 	w http.ResponseWriter,
 	_ *http.Request,
